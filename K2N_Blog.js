8b27173714b1da037754a6963fa16ad4800c5a12
PitchRE <pitch@redvelvet.pro>
2019-07-08 19:56:23 +0200

    fix

--- K2N_Blog.js
+++ K2N_Blog.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
+// @author       Pitch
 // @match        https://k2nblog.com/*
 // @grant        none
 // @match        https://www.mirrored.to/files/*
